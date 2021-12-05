6c25123f779461fa389d9767059aad7b3110f981
QAVRoman <rvakulka@gmail.com>
2021-12-05 16:07:17 +0300

    Reformat code

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,10 @@
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
         int balance = 100; // счет клиента
         int replenishment = 1100; // сумма пополнения счета
-            int bonus = replenishment/100;
-            int amount = balance + replenishment + bonus;
-            System.out.println(" Ваш бонус " + bonus);
-            System.out.println(" Cумма счета " + amount);
+        int bonus = replenishment / 100;
+        int amount = balance + replenishment + bonus;
+        System.out.println(" Ваш бонус " + bonus);
+        System.out.println(" Cумма счета " + amount);
     }
 }
