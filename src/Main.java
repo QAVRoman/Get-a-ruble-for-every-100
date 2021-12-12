72d15755423bae705166e1b4ec78205351d784a8
QAVRoman <rvakulka@gmail.com>
2021-12-12 13:58:06 +0300

    V. 2.0 if else

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         int balance = 100; // счет клиента
-        int replenishment = 1100; // сумма пополнения счета
-        int bonus = replenishment / 100;
+        int replenishment = 150; // сумма пополнения счета
+        int bonus;
+        if (replenishment >= 100) {
+            bonus = replenishment / 100;
+        } else {
+            bonus = 0;
+        }
         int amount = balance + replenishment + bonus;
         System.out.println(" Ваш бонус " + bonus);
         System.out.println(" Cумма счета " + amount);
